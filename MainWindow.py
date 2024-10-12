61921ca7cba1eac0ac27a61d84fd2a4f04227f24
KyleDunbarDev <kyledunbar9000@gmail.com>
2024-10-12 21:34:03 +0100

    Dynamically loads songs and collections from dummy data

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QApplication, QDockWidget, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QLabel
 
 from Collections import *
-
+from Songs import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,11 +13,15 @@ class MainWindow(QMainWindow):
         collections = Collections()
         collections.populate(loaded_collections)
 
+        loaded_songs = [Song("/folder/song1.mp3","Darude Sandstorm", "Weezer"), Song("/folder/song2.mp3", "Never Gonna Give You Up", "Rick Astley")]
+        songs = Songs()
+        songs.populate(loaded_songs)
+
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
         self.setLayout(layout)
         layout.addWidget(collections)
-        #layout.addWidget(songs)
+        layout.addWidget(songs)
         #layout.addWidget(keybinds)
