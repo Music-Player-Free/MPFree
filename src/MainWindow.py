bfb806cfe264bad14465ff13e6b5de503642c858
gubby64 <angushay42@gmail.com>
2024-10-16 14:23:22 +0100

    Button for file. Database initial stuff.

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import (QApplication, QDockWidget, QWidget, QLabel,
                                QMainWindow, QVBoxLayout, QHBoxLayout,
-                               QStackedWidget, QPushButton)
+                               QStackedWidget, QPushButton, QGroupBox, QFileDialog)
 
 from Collections import *
 from Keybinds import KeybindsPane
@@ -47,48 +47,66 @@ class Not_ify(QMainWindow):
         super().__init__()
         self.label = QLabel("MPFree Music Player")
         
-        loaded_collections = [Collection("My Playlist"), Collection("Cool Album")] # TODO: replace with a list, loaded from database
-        collections = Collections()
-        collections.populate(loaded_collections)
+        # loaded_collections = [Collection(0,"My Playlist"), Collection(1, "Cool Album")] # TODO: replace with a list, loaded from database
+        # collections = Collections()
+        # collections.populate(loaded_collections)
 
         songs = Songs()
         loaded_songs = [Song(0, "/folder/song1.mp3", "song 1", "sample", 300), Song(1, "/folder/song2.mp3", "Never Gonna Give You Up", "Rick Astley", 302)]
         songs.populate(loaded_songs)
-
+        
         # Vertical layout.
         layout = QVBoxLayout()
+
+
         
         # Set background "outline" widget
-        outline_widget = QWidget()
-        outline_widget.setLayout(layout) # apply vertical layout to this also
-        self.setCentralWidget(outline_widget)  # set as central widget for others to reside in.
+        outline_widget = QGroupBox()
+
+        settings = QWidget()
+
+
+        # TODO:
+        # resize button
+        # add text input also.
+        file_button = QPushButton("Choose source folder")
+        file_button.clicked.connect(self.button_file)
+
+        
+
+        layout2 = QVBoxLayout()
+        layout2.addWidget(file_button)
+        settings.setLayout(layout2)
 
         # Create button to swap stack
         button = QPushButton("here")
         button.clicked.connect(self.clickable)
-        layout.addWidget(button)
 
         # Create stacked widget for songs/collections
         stacked = QStackedWidget()
         stacked.addWidget(songs)
-        stacked.addWidget(collections)
+        stacked.addWidget(settings)
 
+        layout.addWidget(button)
         layout.addWidget(stacked)
 
 
-        # self.setCentralWidget(stacked)
+        outline_widget.setLayout(layout) # apply vertical layout to this also
+        self.setCentralWidget(outline_widget)  # set as central widget for others to reside in.
         
+
         # TODO
         # THIS IS BAD BROS OMG !!!!1! 
         # No dynamicism, hard-coded.
         self.widgets = {}
-        self.widgets['collections'] = stacked.indexOf(collections)
+        self.widgets['settings'] = stacked.indexOf(settings)
         self.widgets['songs'] = stacked.indexOf(songs)
-
-
-        print(self.widgets)
-        
-
+        # self.widgets['collections'] = stacked.indexOf(collections)
+       
+    def button_file(self):
+        dialog = QFileDialog()
+        file = dialog.getExistingDirectory(None, "Choose folder")
+        return file
 
     def clickable(self):
         widgets = self.centralWidget().children()
--- src/db.py
+++ src/db.py
@@ -1,28 +1,59 @@
 import sqlite3
 
 
-def connect():
-    conn=sqlite3.connect("songs.db")
-    cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (song_id INTEGER PRIMARY KEY, song_name TEXT NOT NULL, track_len INTEGER NOT NULL, artist TEXT NOT NULL, album TEXT NOT NULL)")
-    conn.commit()
-    conn.close()
+class Database:
+    # TODO: 
+    # File organisation, path might change.
+    DB_PATH = "mpfree.db"
+    def __init__(self, user_filename):
+        # set up connection
+        self.con = sqlite3.connect(self.DB_PATH)
+        self.cur = self.con.cursor()
 
-def insert(name,track_len,artist,album):
-    conn=sqlite3.connect("songs.db")
-    cur=conn.cursor()
-    cur.execute("INSERT INTO book (song_name, track_len, artist, album) VALUES (?,?,?,?)", (name,track_len,artist,album))
-    conn.commit()
-    conn.close()
+        self.user_filename = user_filename
 
-def view():
-    conn=sqlite3.connect("songs.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM book ")
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    def create(self):
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, song_name TEXT NOT NULL,track_len INTEGER NOT NULL, artist TEXT NOT NULL,album TEXT NOT NULL);
+                        ''')
+        
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS tags (id INT PRIMARY_KEY NOT NULL, name TEXT NOT NULL,colour TEXT NOT NULL);
+                        ''')
+        
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS collections  (id INT PRIMARY KEY NOT NULL,name TEXT NOT NULL,description TEXT NOT NULL,author);
+                        ''')
 
-connect()
-insert('Bombtrack', 340, 'RATM', 'RATM')
-print(view())
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS songs_tags (idx INT PRIMARY KEY NOT NULL, songs_id INT NOT NULL, tags_id INT NOT NULL, FOREIGN KEY(songs_id) REFERENCES songs(id), FOREIGN KEY(tags_id) REFERENCES tags(id),);
+                        ''')
+        
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS songs_collections (idx INT PRIMARY KEY NOT NULL, songs_id INT NOT NULL, collections_id INT NOT NULL, FOREIGN KEY(songs_id) REFERENCES songs(id), FOREIGN KEY(collections_id) REFERENCES songs(id));
+                        ''')
+        
+        self.cur.execute('''
+                        CREATE TABLE IF NOT EXISTS collections_tags(idx INT PRIMARY KEY NOT NULL, collections_id INT NOT NULL, tags_id INT NOT NULL, FOREIGN KEY(collections_id) REFERENCES collections(id), FOREIGN KEY(tags_id) REFERENCES tags(id));
+                        ''')
+
+    def insert(self, data):
+        pass
+
+
+    # Context manager stuff
+    def __enter__(self):
+        return self
+
+    # Context manager cont.
+    # Maybe use ext_type & traceback
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.cur.close()
+        if isinstance(exc_value, Exception):
+            self.con.rollback()
+        else:
+            self.con.commit()
+        self.con.close()
+
+with Database() as db:
+    db.create()
