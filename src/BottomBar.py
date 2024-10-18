e261cbd3b1916bceb35614f3f0dfa29e55295943
gubby64 <angushay42@gmail.com>
2024-10-18 23:00:58 +0100

    More comments. Need to establish convention of extending QT classes: Create layout Create labels, widgets, own widgets etc. Apply widgets to layout Apply layout to self, or whatever we apply to
    I know the order doesn't matter, but when commenting I found this to be the most readable, so if there is a better way then we can use that way.
    
    Also, I have used docstrings (first multi-line comment ''' under any class/function etc) to add TODO lists with markdown.

--- src/BottomBar.py
+++ src/BottomBar.py
@@ -4,60 +4,97 @@ from PySide6.QtWidgets import QVBoxLayout, QWidget, QHBoxLayout, QLabel, QPushBu
 from ToggleWidget import ToggleWidget
 
 class BottomBar(QWidget):
-    def __init__(self, ref: ToggleWidget):
+    def __init__(self, ref: 'ToggleWidget'):
         super().__init__()
         layout = QHBoxLayout()
-        self.setLayout(layout)
 
+        # Instantiate NowPlaying object
         now_playing = NowPlaying()
         layout.addWidget(now_playing)
 
+        # Instantiate MediaControl object
         media_controls = MediaControls()
         layout.addWidget(media_controls)
 
+        # Instantiate SettingsButton object
         settings_button = SettingsButton(ref)
         layout.addWidget(settings_button)
 
+        self.setLayout(layout)
 
 
 
 class NowPlaying(QWidget):
+    '''
+    Extends QWidget and shows currently playing song <br>
+    and artist.
+    '''
 
-    def __init__(self):#TODO: Should take string as args for label texts
+
+    #TODO asf
+    # Should take string as args for label texts 
+    # Remember last song played and position
+    def __init__(self):
         super().__init__()
+        # Using Vertical layout.
         layout = QVBoxLayout()
-        self.setLayout(layout)
 
+        # Create label, set text and add to vertical layout
         song = QLabel()
         song.setText("Song Name")
         layout.addWidget(song)
 
+        # Create label, set text and add to vertical layout
         artist = QLabel()
         artist.setText("Artist")
         layout.addWidget(artist)
 
-class MediaControls(QWidget):
-    def  __init__(self):
-        super().__init__()
-        layout = QHBoxLayout()
+        # Apply newly made layout to self(Qwidget that represents now playing area)
         self.setLayout(layout)
 
-        shuffle_button = QPushButton()#TODO: Connect buttons up to slots/methods
+class MediaControls(QWidget):
+    '''
+    ### TODO: 
+    <ul>
+        <li>Shuffle button to randomize current collection. Should collections remember shuffle preference?</li>
+        <li>Skip forward</li>
+        <li>Skip backward</li>
+    </ul>
+
+    Media controls widget extending QWidget \n
+    Shuffle, play/pause, skip back and forwards.
+    '''
+    def  __init__(self):
+        
+        super().__init__()
+        # Using Horizontal layout
+        layout = QHBoxLayout()
+        
+
+        #TODO: Connect buttons up to slots/methods!!!!
+
+        # Instantiate button with text, add to layout template
+        shuffle_button = QPushButton() 
         shuffle_button.setText("Shuffle")
         layout.addWidget(shuffle_button)
 
+        # Instantiate button with text, add to layout template
         prev_button = QPushButton()
         prev_button.setText("Previous")
         layout.addWidget(prev_button)
 
+        # Instantiate button with text, add to layout template
         play_button = QPushButton()
         play_button.setText("Play / Pause")
         layout.addWidget(play_button)
 
+        # Instantiate button with text, add to layout template
         next_button = QPushButton()
         next_button.setText("Next")
         layout.addWidget(next_button)
 
+        # Apply buttons to layout template
+        self.setLayout(layout)
 
     #TODO
     def toggle_play_pause(self):
@@ -70,37 +107,61 @@ class MediaControls(QWidget):
 
     #TODO
     def previous(self):
-        #if current timestamp <5s
+        #if current timestamp <2s
         print("previous song loaded")
         #else:
             #print("song restarted") // set time to 0
 
 
 class PlaybackControls(QWidget):
+    '''    
+    Playback controls extends QWidget.
+    '''
     def __init__(self):
         super().__init__()
+        # Create layout
         layout = QHBoxLayout()
-        self.setLayout(layout)
 
+        # Create button
         button = QPushButton()
         button.setText("Playback\nControls")
 
+        # Add button to layout template
         layout.addWidget(button)
+
+        # apply template to qwidget
+        self.setLayout(layout)
 
 class SettingsButton(QWidget):
-    def __init__(self, ref: ToggleWidget):
+    '''
+    ### TODO: 
+    <ul>
+        <li>Layout of settings page</li>
+        <li>Add appropriate widgets of settings. </li>
+    </ul>
+
+    Replace central widgets with settings page.
+    '''
+    def __init__(self, ref: 'ToggleWidget'):
         super().__init__()
+        # Create layout template
         layout = QHBoxLayout()
-        self.setLayout(layout)
+
+        # Better name for this variable?
         self.ref = ref
 
+        # Create, label and connect button
         button = QPushButton()
         button.setText("Settings")
-        button.clicked.connect(self.clickable)
+        button.clicked.connect(self.ref.toggle)
 
-        playback_button =  PlaybackControls()
-        layout.addWidget(playback_button)
+        # Create playback widiget
+        playback_widget =  PlaybackControls()
+
+        # Add to layout template
+        layout.addWidget(playback_widget)
         layout.addWidget(button)
 
-    def clickable(self):
-        self.ref.toggle()
+        # Apply layout
+        self.setLayout(layout)
+
