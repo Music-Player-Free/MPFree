d2d60d331b2926f6460945214c33465a14ea163e
gubby64 <angushay42@gmail.com>
2024-11-10 19:29:47 +0000

    new file to start work with

---
+++ src/queueMPF.py
@@ -0,0 +1,10 @@
+'''
+TODO:
+- doubly linked list + hmp for O(1) operations and standard FIFO stuff
+- needs to interact with player somehow? Maybe doesn't, need more BP (brainpower)
+- Look into drag & drop stuff
+    - design widgets with this in mind
+- queue items need to have dialog boxes
+- history? See mind map.
+
+'''
